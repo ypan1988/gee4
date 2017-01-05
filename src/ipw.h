def7adfc684fc4c3d166469552a2472b48c40890
Yi Pan <ypan1988@gmail.com>
2017-01-05 16:21:12 +0000

    update ipw

--- src/ipw.h
+++ src/ipw.h
@@ -10,7 +10,7 @@ namespace gee {
   ipw(const arma::uvec &m, const arma::vec &Y, arma::uword order)
     : m_(m), Y_(Y), order_(order) { }
 
-    inline arma::vec get_Y(const arma::uword i) const {
+    arma::vec get_Y(const arma::uword i) const {
       arma::vec Yi;
       if (i == 0)
         Yi = Y_.subvec(0, m_(0) - 1);
@@ -20,21 +20,34 @@ namespace gee {
       }
       return Yi;
     }
-        
+
+    arma::vec get_Z(const arma::uword i, const arma::uword j) const {
+      arma::vec Yi = get_Y(i);
+      arma::vec Z_ij = arma::zeros<arma::vec>(order_+1);
+      if ((j+1) <= order_) Z_ij(0) = 1;
+      else {
+        Z_ij(0) = 1;
+        for (auto k = 1; k <= order_; ++k) Z_ij(k) = Yi(j - k); 
+      }
+
+      return Z_ij;
+    }
+    
     arma::vec operator()(const arma::vec &alpha) {
       arma::uword nsub = m_.n_elem;
       arma::uword npar = alpha.n_elem;
 
       arma::vec result = arma::zeros<arma::vec>(npar);
       for (auto i = 0; i != nsub; ++i) {
-        arma::vec Yi = get_Y(i);
+        //arma::vec Yi = get_Y(i);
         for (auto j = 1; j != m_(i); ++j) {
-          arma::vec Z_ij = arma::zeros<arma::vec>(npar);
-          if ((j+1) <= order_) Z_ij(0) = 1;
-          else {
-            Z_ij(0) = 1;
-            for (auto k = 1; k <= order_; ++k) Z_ij(k) = Yi(j - k); 
-          } 
+          arma::vec Z_ij = get_Z(i, j);
+          /* arma::vec Z_ij = arma::zeros<arma::vec>(npar); */
+          /* if ((j+1) <= order_) Z_ij(0) = 1; */
+          /* else { */
+          /*   Z_ij(0) = 1; */
+          /*   for (auto k = 1; k <= order_; ++k) Z_ij(k) = Yi(j - k);  */
+          /* }  */
           
           double tmp = arma::as_scalar(arma::exp(Z_ij.t() * alpha));
           double p_ij =  tmp / (1 + tmp);
@@ -54,33 +67,36 @@ namespace gee {
       arma::uword index = 0;
       arma::vec result = arma::zeros<arma::vec>(Y_.n_elem);
       for (auto i = 0; i != nsub; ++i) {
-	arma::vec Yi = get_Y(i);
-	arma::vec p_i = arma::zeros<arma::vec>(m_(i));
-	arma::vec Pi_i = arma::zeros<arma::vec>(m_(i));
-	for (auto j = 0; j != m_(i); ++j, ++index) {
-	  
-	  if (j == 0) {
-	    p_i(0) = 0;
-	    Pi_i(0) = 1 - p_i(0);
-	    result(index) = 1 / Pi_i(0);
-	  } else {
-	    arma::vec Z_ij = arma::zeros<arma::vec>(npar);
-	    if ((j+1) <= order_) Z_ij(0) = 1;
-	    else {
-	      Z_ij(0) = 1;
-	      for (auto k = 1; k <= order_; ++k) Z_ij(k) = Yi(j - k); 
-	    } 
-	    
-	    double tmp = arma::as_scalar(arma::exp(Z_ij.t() * alpha));
-	    double p_ij =  tmp / (1 + tmp);
-	    p_i(j) = p_ij;
-	    Pi_i(j) = Pi_i(j-1) * (1 - p_ij);
-	    result(index) = result(index-1) / Pi_i(j);
-	  }
+        //arma::vec Yi = get_Y(i);
+        arma::vec p_i = arma::zeros<arma::vec>(m_(i));
+        arma::vec Pi_i = arma::zeros<arma::vec>(m_(i));
+        for (auto j = 0; j != m_(i); ++j, ++index) {
+          
+          if (j == 0) {
+            p_i(0) = 0;
+            Pi_i(0) = 1 - p_i(0);
+            result(index) = 1 / Pi_i(0);
+          } else {
+            arma::vec Z_ij = get_Z(i, j);
+	    // if (debug && j == 1) Z_ij.t().print("\nZ_ij = ");
+            /* arma::vec Z_ij = arma::zeros<arma::vec>(npar); */
+            /* if ((j+1) <= order_) Z_ij(0) = 1; */
+            /* else { */
+            /*   Z_ij(0) = 1; */
+            /*   for (auto k = 1; k <= order_; ++k) Z_ij(k) = Yi(j - k);  */
+            /* }  */
+            
+            double tmp = arma::as_scalar(arma::exp(Z_ij.t() * alpha));
+	    if (debug && j == 1)  Rcpp::Rcout << "tmp = " << tmp << std::endl;
+            double p_ij =  tmp / (1 + tmp);
+            p_i(j) = p_ij;
+            Pi_i(j) = Pi_i(j-1) * (1 - p_ij);
+            result(index) = result(index-1) / Pi_i(j);
+          }
 
-	  if (debug) Rcpp::Rcout << index << ": " << result(index) << " ";  
-	} // for loop j
-	if (debug) Rcpp::Rcout << std::endl;
+          if (debug) Rcpp::Rcout << index << ": " << result(index) << " ";  
+        } // for loop j
+        if (debug) Rcpp::Rcout << std::endl;
       } // for loop i
       return result;
     } 
